1defc507ff23c0fb0bc48621f43b8e26298aa4a7
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2024-01-04 22:15:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import OrderService from "./components/OrderService";
+import { BiPlus } from "react-icons/bi";
+import data from "./db.json"
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [submittedData, setSubmittedData] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+
+   <div className="flex flex-col items-center">
+
+{/****************************************       Displaying Dummy Data on Submit    *****************************************/}
+
+      <p className="text-lg font-bold pt-5 pb-2">{submittedData ? "Orders" : "No ordering services here."}</p>
+      {submittedData && <OrderService setOpenModal={setModalOpen} setSubmitted={setSubmittedData} /> ?(
+        <div className="overflow-x-auto rounded-lg border border-gray-200 mb-5">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Name</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Nodes</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Channel Name</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Domain</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Message</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Actual Bytes</th>
+              <th className="whitespace-nowrap px-4 py-2 font-bold text-gray-900">Preffered Bytes</th>
+            </tr>
+          </thead>
+      
+          <tbody className="divide-y divide-gray-200">
+          {data.map((item,i)=>{
+                    return <tr key={i}>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.name}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.nodes}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.cname}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.domain}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.message}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.actualbytes}</td>
+                      <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{item.prefferedbytes}</td>
+                    </tr>
+                  })}
+          </tbody>
+        </table>
+      </div>)
+        :<p className="text-sm text-gray-400 pb-5" >Services list will be listed here.</p>}
+
+{/********************************        Displaying Dummy Data on Submit Ends Here       *********************************/}
+
+      <div
+        className="w-[250px] h-[60px] flex gap-4 rounded-lg cursor-pointer bg-blue-800 text-white"
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+      <BiPlus className="mt-4 ml-2" size={25} /> 
+      <p className="mt-4 mx-2 text-lg">Add Ordering Service</p>
+      </div>
+      
+        
+      {modalOpen && <OrderService setOpenModal={setModalOpen} setSubmitted={setSubmittedData}  />}
     </div>
+   </>
   );
 }
 
---
+++ src/components/OrderService.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { BiChevronDown, BiChevronUp, BiSolidXSquare } from "react-icons/bi";
+
+function OrderService({ setOpenModal, setSubmitted }) {
+    const [status, setStatus] = useState(false);
+
+    return (
+        <div className="w-[100vw] h-[100vh] bg-white fixed flex justify-center items-center">
+            <div className={status ? "w-[900px] h-[600px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]" : "w-[900px] h-[400px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]"}>
+                <div onClick={() => setOpenModal(false)} className="flex justify-end">
+                    <BiSolidXSquare className="text-orange-100 bg-red-500 cursor-pointer" size={30} />
+                </div>
+                <div className="inline-block text-lg font-semibold pb-5 mt-[-2rem] ml-[10px]">
+                    <h1>Create Ordering Service</h1>
+                </div>
+                <div className="flex justify-around">
+                    <div className="flex flex-col">
+                        <label className="block text-md font-medium text-gray-400"> Organization Name <span className="text-red-500">*</span> </label>
+                        <input
+                            type="text"
+                            id="name"
+                            required
+                            placeholder="Organization Name 1"
+                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="block text-md font-medium text-gray-400"> No. of Ordering Nodes <span className="text-red-500">*</span> </label>
+                        <input
+                            type="text"
+                            id="nodes"
+                            required
+                            placeholder="Organization Name 1"
+                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                        />
+                    </div>
+                </div>
+                <div className="flex justify-around mt-6">
+                    <div className="flex flex-col">
+                        <label className="block text-md font-medium text-gray-400"> Channel Name (Optional) </label>
+                        <input
+                            type="text"
+                            id="cname"
+                            placeholder="Organization Name 1"
+                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="block text-md font-medium text-gray-400"> Domain <span className="text-red-500">*</span></label>
+                        <input
+                            type="text"
+                            id="domain"
+                            required
+                            placeholder="Organization Name 1"
+                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                        />
+                    </div>
+                </div>
+
+                {/****************************************     Advanced Configuration Toggle View     *************************************************************/}
+
+                <div className="text-lg font-semibold pb-5 mt-[2rem] ml-[10px]">
+                    <div onClick={() => setStatus(!status)} className="flex cursor-pointer">
+                        <h1>Advanced Configurations</h1>
+                        {!status ? <BiChevronDown className="ml-3" size={30} /> : <BiChevronUp className="ml-3" size={30} />}
+                    </div>
+                </div>
+                {status ?
+                    (<div className="block h-[50vh]">
+                        <div className="flex justify-around">
+                            <div className="flex flex-col">
+                                <label className="block text-md font-medium text-gray-400"> Max Message Count <span className="text-red-500">*</span> </label>
+                                <input
+                                    type="text"
+                                    id="message"
+                                    required
+                                    placeholder="E.g.10"
+                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <label className="block text-md font-medium text-gray-400"> Absolute Max Bytes </label>
+                                <input
+                                    type="text"
+                                    id="actualbytes"
+                                    placeholder="E.g.1038723"
+                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex justify-start mt-6 ml-3">
+                            <div className="flex flex-col justify-start">
+                                <label className="block text-md font-medium text-gray-400"> Preffered Max Bytes <span className="text-red-500">*</span> </label>
+                                <input
+                                    type="text"
+                                    id="prefferedbytes"
+                                    required
+                                    placeholder="E.g.524288"
+                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                />
+                            </div>
+                        </div>
+                    </div>) : null
+                }
+
+                {/* **************************************************      End of Advanced Configuration Toggle View     ***************************************************/}
+
+                <div className="flex justify-end items-center gap-3">
+                    <button
+                        className="bg-blue-100 text-blue-600 py-2 px-7 rounded-lg font-medium"
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+
+                    >
+                        Cancel
+                    </button>
+                    <button className="bg-blue-800 text-white py-2 px-7 rounded-lg font-medium cursor-pointer" onClick={() => {
+                        setSubmitted(true);
+                        setOpenModal(false);
+                    }}>Submit</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default OrderService;
