472c424ec4133ac4a6976adbcecf645cdde91c2f
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2024-01-04 22:34:52 +0530

    added mobile responsiveness

--- src/components/OrderService.jsx
+++ src/components/OrderService.jsx
@@ -5,15 +5,15 @@ function OrderService({ setOpenModal, setSubmitted }) {
     const [status, setStatus] = useState(false);
 
     return (
-        <div className="w-[100vw] h-[100vh] bg-white fixed flex justify-center items-center">
-            <div className={status ? "w-[900px] h-[600px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]" : "w-[900px] h-[400px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]"}>
+        <div className="lg:w-[100vw] lg:h-[100vh] h-full overflow-y-auto bg-white fixed flex justify-center items-center">
+            <div className={status ? "lg:w-[900px] lg:h-[600px] w-[360px] h-[900px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]" : "lg:w-[900px] lg:h-[400px] w-[360px] h-[600px] rounded-md bg-white flex flex-col p-[25px] shadow-[0_5px_15px_rgba(0,0,0,0.35)]"}>
                 <div onClick={() => setOpenModal(false)} className="flex justify-end">
                     <BiSolidXSquare className="text-orange-100 bg-red-500 cursor-pointer" size={30} />
                 </div>
                 <div className="inline-block text-lg font-semibold pb-5 mt-[-2rem] ml-[10px]">
                     <h1>Create Ordering Service</h1>
                 </div>
-                <div className="flex justify-around">
+                <div className="flex flex-col lg:flex-row justify-around">
                     <div className="flex flex-col">
                         <label className="block text-md font-medium text-gray-400"> Organization Name <span className="text-red-500">*</span> </label>
                         <input
@@ -21,7 +21,7 @@ function OrderService({ setOpenModal, setSubmitted }) {
                             id="name"
                             required
                             placeholder="Organization Name 1"
-                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                            className="mt-1 lg:w-[25rem] w-[20rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                         />
                     </div>
                     <div className="flex flex-col">
@@ -31,18 +31,18 @@ function OrderService({ setOpenModal, setSubmitted }) {
                             id="nodes"
                             required
                             placeholder="Organization Name 1"
-                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                            className="mt-1 lg:w-[25rem] w-[20rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                         />
                     </div>
                 </div>
-                <div className="flex justify-around mt-6">
+                <div className="flex flex-col lg:flex-row justify-around mt-6">
                     <div className="flex flex-col">
                         <label className="block text-md font-medium text-gray-400"> Channel Name (Optional) </label>
                         <input
                             type="text"
                             id="cname"
                             placeholder="Organization Name 1"
-                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                            className="mt-1 lg:w-[25rem] w-[20rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                         />
                     </div>
                     <div className="flex flex-col">
@@ -52,7 +52,7 @@ function OrderService({ setOpenModal, setSubmitted }) {
                             id="domain"
                             required
                             placeholder="Organization Name 1"
-                            className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                            className="mt-1 lg:w-[25rem] w-[20rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                         />
                     </div>
                 </div>
@@ -67,7 +67,7 @@ function OrderService({ setOpenModal, setSubmitted }) {
                 </div>
                 {status ?
                     (<div className="block h-[50vh]">
-                        <div className="flex justify-around">
+                        <div className="flex lg:flex-row flex-col justify-around">
                             <div className="flex flex-col">
                                 <label className="block text-md font-medium text-gray-400"> Max Message Count <span className="text-red-500">*</span> </label>
                                 <input
@@ -75,7 +75,7 @@ function OrderService({ setOpenModal, setSubmitted }) {
                                     id="message"
                                     required
                                     placeholder="E.g.10"
-                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                    className="mt-1 lg:w-[25rem] w-[20rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                                 />
                             </div>
                             <div className="flex flex-col">
@@ -84,11 +84,11 @@ function OrderService({ setOpenModal, setSubmitted }) {
                                     type="text"
                                     id="actualbytes"
                                     placeholder="E.g.1038723"
-                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                    className="mt-1 lg:w-[25rem] w-[20rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                                 />
                             </div>
                         </div>
-                        <div className="flex justify-start mt-6 ml-3">
+                        <div className="flex justify-start mt-6 lg:ml-3">
                             <div className="flex flex-col justify-start">
                                 <label className="block text-md font-medium text-gray-400"> Preffered Max Bytes <span className="text-red-500">*</span> </label>
                                 <input
@@ -96,7 +96,7 @@ function OrderService({ setOpenModal, setSubmitted }) {
                                     id="prefferedbytes"
                                     required
                                     placeholder="E.g.524288"
-                                    className="mt-1 w-[25rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
+                                    className="mt-1 lg:w-[25rem] w-[20rem] h-[3rem] p-5 rounded-md border-gray-200 shadow-sm sm:text-sm border bottom-1"
                                 />
                             </div>
                         </div>
